15f545bd613a234c6dfcbf36ba7e95fd59b30f94
Yaron Shamir <temp@yaronshamir.com>
2023-07-26 16:49:46 +1000

    updated examples to layouts and components

---
+++ components/factory.js
@@ -0,0 +1,21 @@
+// auto generated list of available components
+
+const components = {
+'Paragraph': require('./Paragraph/Paragraph.js')
+};
+
+function get(key) {
+   if (components[key] == null) {
+     console.log('Component ' +key + ' not found')
+      return new components['Component']()
+   }
+
+   return {
+      ...require('./component'),
+      ...components[key]
+   }
+}
+
+module.exports = {
+   get
+};
